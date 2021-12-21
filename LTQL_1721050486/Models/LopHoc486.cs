17849e8bcc33c802e506cd6c8222f54c4f6f71c2
DESKTOP-RAAIMNI\ACER <exanp>
2021-12-21 13:03:15 +0700

    Create_Table_LopHoc_DDT486

---
+++ LTQL_1721050486/Models/LopHoc486.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050486.Models
+{
+    public class LopHoc486
+    {
+        [Key]
+        [Display(Name = "Mã Lớp")]
+        public int Malop { get; set; }
+        [Display(Name = "Tên Lớp")]
+        [StringLength(50)]
+        public string TenLop { get; set; }
+    }
+}
